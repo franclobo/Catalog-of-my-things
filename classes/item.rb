b4c295ec559da3326dceaf2de652dd1c45fd87a1
AntonioHincapie <marcohincapiem@gmail.com>
2022-08-22 14:43:59 -0500

    Add setters to the variables and ad can_be_archive method

--- classes/item.rb
+++ classes/item.rb
@@ -1,15 +1,29 @@
 class item
-  attr_accessor :genre, :author, :source, :label, :publish_date
-  attr_reader :id, :archived
+  attr_accessor :publish_date
+  attr_reader :id, :genre, :author, :label, :archived
 
-  def initialize (id, genre, author, source, label, publish_date, archived)
-    @id = id
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
-    @publish_date = date
-    @archived = false
+  def initialize (id, publish_date, archived: false)
+    @id = Random.rand(1..1000)
+    @publish_date = publish_date
+    @archived = archived
   end
 
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def label=(label)
+    @label = label
+  end
+
+  def can_be_archive?
+    @publish_date.parse > @publish_date.parse.prev_year(10)
+  end
+
+  def move_to_archive   
+  end
 end
