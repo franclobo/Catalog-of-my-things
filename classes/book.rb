2e4e896dffdd7784a2654566f4b3cb25eb6a59b6
AntonioHincapie <marcohincapiem@gmail.com>
2022-08-23 16:38:00 -0500

    Create JSON object to book class

--- classes/book.rb
+++ classes/book.rb
@@ -1,12 +1,43 @@
 require_relative './item'
+require_relative './label'
 
 class Book < Item
+  attr_reader :id
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state, publish_date)
-    super(publish_date)
+  # rubocop:disable Metrics/ParameterLists
+  def initialize(
+    publisher, cover_state, publish_date, label = nil, author = nil, genre = nil
+  )
+    super(label, publish_date, author, genre)
     @publisher = publisher
     @cover_state = cover_state
+  end
+
+  # rubocop:enable Metrics/ParameterList
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'title' => @label.title,
+      'publisher' => @publisher,
+      'cover_state' => @cover_state,
+      'publish_date' => @publish_date,
+      'author' => @author,
+      'genre' => @genre
+    }.to_json(*args)
+  end
+
+  def self.json_create(book)
+    new(
+      book['id'],
+      Label.new(book['title']),
+      book['publisher'],
+      book['cover_state'],
+      book['publish_date'],
+      book['author'],
+      book['genre']
+    )
   end
 
   private
