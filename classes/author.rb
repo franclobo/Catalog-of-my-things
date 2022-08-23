482fbac643407e1f033e7e10dce00de1f3de0279
franclobo <fjbl2788@gmail.com>
2022-08-23 12:33:43 -0500

    Add json files to preserve data from classes Author and Games.

--- classes/author.rb
+++ classes/author.rb
@@ -13,4 +13,19 @@ class Author
     items << item unless items.include?(item)
     item.author(self)
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'a' => [id, first_name, last_name, items]
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    id, first_name, last_name, items = object['a']
+    author = new(first_name, last_name)
+    author.id = id
+    author.items = items
+    author
+  end
 end
--- classes/game.rb
+++ classes/game.rb
@@ -9,6 +9,24 @@ class Game < Item
     @last_played_at = last_played_at
   end
 
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'a' => [id, publish_date, last_played_at, multiplayer, archived, genre&.id, label&.id, author&.id]
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    id, publish_date, last_played_at, multiplayer, archived, genre_id, label_id, author_id = object['a']
+    game = new(publish_date, last_played_at, multiplayer: multiplayer)
+    game.id = id
+    game.archived = archived
+    game.genre = genre_id
+    game.label = label_id
+    game.author = author_id
+    game
+  end
+
   private
 
   def can_be_archive?
