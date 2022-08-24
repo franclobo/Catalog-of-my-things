e8804c383470517b2c3f2f9526d91e5b1f583048
franclobo <fjbl2788@gmail.com>
2022-08-24 09:39:58 -0500

    Edit author test

--- classes/spec/author_spec.rb
+++ classes/spec/author_spec.rb
@@ -2,28 +2,26 @@ require_relative '../author'
 
 describe Author do
   context 'when initialized' do
+    before(:all) do
+      @author = Author.new('John', 'Doe')
+    end
+
     it 'should have an id' do
-      author = Author.new('John', 'Doe')
-      expect(author.id).to be_a Integer
+      expect(@author.id).to be_a Integer
     end
     it 'should have a first name' do
-      author = Author.new('John', 'Doe')
-      expect(author.first_name).to eq 'John'
+      expect(@author.first_name).to eq 'John'
     end
     it 'should have a last name' do
-      author = Author.new('John', 'Doe')
-      expect(author.last_name).to eq 'Doe'
+      expect(@author.last_name).to eq 'Doe'
     end
-  end
 
-  context 'when adding items' do
     it 'should add an item' do
-      author = Author.new('John', 'Doe')
       item = double('Item')
       allow(item).to receive(:author)
-      expect(item).to receive(:author).with(author)
-      author.add_item(item)
-      expect(author.items).to eq [item]
+      expect(item).to receive(:author).with(@author)
+      @author.add_item(item)
+      expect(@author.items).to eq [item]
     end
   end
 end
