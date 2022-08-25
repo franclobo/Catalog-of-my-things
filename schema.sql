0413d7a39ef034a1fb3155564dfb509c991a60b4
AntonioHincapie <marcohincapiem@gmail.com>
2022-08-25 15:50:58 -0500

    Add squema.sql for label and book classes

--- schema.sql
+++ schema.sql
@@ -80,3 +80,34 @@ ALTER TABLE IF EXISTS public."Author"
 
 COMMENT ON TABLE public."Author"
     IS 'Create table Author';
+
+CREATE TABLE public."Book"
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+    publisher text NOT NULL,
+    cover_state boolean NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public."Book"
+    OWNER to postgres;
+
+COMMENT ON TABLE public."Book"
+    IS 'Create table Book';
+
+CREATE TABLE public."Label"
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+    title text NOT NULL,
+    color text NOT NULL,
+    items integer NOT NULL,
+    PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."Label"
+    OWNER to postgres;
+
+COMMENT ON TABLE public."Label"
+    IS 'Create table Label';
